012a6fa7a478093033e47b7f2b3794b784dbf46b
666 <comrainy@gmail.com>
2022-11-24 08:57:08 +0300

    zadacha23

--- Program.cs
+++ Program.cs
@@ -51,5 +51,32 @@ Console.WriteLine($"Длина отрезка  {segmentLength}");
 
 void Zadacha23()
 {
+    Console.Write("Введите число: ");
+    int cube = Convert.ToInt32(Console.ReadLine());
 
+    void Cube(int[] cube)
+    {
+    int counter = 0;
+    int length = cube.Length;
+    while (counter <  length)
+    {
+        cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+    }
+
+    void PrintArry(int[] coll)
+    {
+    int count = coll.Length;
+    int index = 0;
+    while(index < count)
+    {
+        Console.Write(coll[index]+ " ");
+        index++;
+    }
+    } 
+
+    int[] arry = new int[cube+1];
+    Cube(arry);
+    PrintArry(arry);
 }
